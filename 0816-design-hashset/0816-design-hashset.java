f549e870ab52b2b0a8b6aefd2d37daa841b8bbd6
Pratham Singh <112713687+pratham1singh@users.noreply.github.com>
2023-05-30 16:11:24 +0530

    Time: 19 ms (50.28%), Space: 53.5 MB (5.94%) - LeetHub

---
+++ 0816-design-hashset/0816-design-hashset.java
@@ -0,0 +1,27 @@
+class MyHashSet {
+    int arr[];
+    public MyHashSet() {
+        int n=(int)Math.pow(10,6);
+        arr=new int[n+1];
+    }
+    
+    public void add(int key) {
+       arr[key]=1;
+    }
+    
+    public void remove(int key) {
+       arr[key]=0;
+    }
+    
+    public boolean contains(int key) {
+        return arr[key]>0;
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
