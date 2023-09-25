d1eab57975b46ce054ff5e028d7c0e432661d732
Pratham Singh <112713687+pratham1singh@users.noreply.github.com>
2023-09-25 18:19:39 +0530

    Time: 531 ms (50.17%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,4 +1,4 @@
 select ifnull(
     (select distinct salary  from Employee order by salary desc limit 1 offset 1 )
     ,null
-) as SecondHighestSalary from dual;
+) as SecondHighestSalary from Employee limit 1 ;
