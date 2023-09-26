a4ee0acbef3c36509bcdb9d39d2e7d03ae0681b7
Pratham Singh <112713687+pratham1singh@users.noreply.github.com>
2023-09-26 18:13:45 +0530

    Time: 1 ms (19%), Space: 43.4 MB (89.44%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.java
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.java
@@ -14,30 +14,27 @@
  * }
  */
 class Solution {
-    public TreeNode make(TreeNode root1,TreeNode root2){
-        if(root1==null&&root2==null)
+    
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+       if(root1==null&&root2==null)
             return null;
         else if(root1==null&&root2!=null){
             TreeNode x= new TreeNode(root2.val);
-            x.left=make(root1,root2.left);
-            x.right=make(root1,root2.right);
+            x.left=mergeTrees(root1,root2.left);
+            x.right=mergeTrees(root1,root2.right);
             return x;
         }
         else if(root1!=null&&root2==null){
             TreeNode x= new TreeNode(root1.val);
-            x.left=make(root1.left,root2);
-            x.right=make(root1.right,root2);
+            x.left=mergeTrees(root1.left,root2);
+            x.right=mergeTrees(root1.right,root2);
             return x;
         }
         else{
             TreeNode x = new TreeNode(root1.val+root2.val);
-            x.left=make(root1.left,root2.left);
-            x.right=make(root1.right,root2.right);
+            x.left=mergeTrees(root1.left,root2.left);
+            x.right=mergeTrees(root1.right,root2.right);
             return x;
         }
-        
-    }
-    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
-        return make(root1,root2);
     }
 }
