cd4cdeae071062a884c646e2544a234918836a2a
Pratham Singh <112713687+pratham1singh@users.noreply.github.com>
2023-10-03 18:35:19 +0530

    Time: 1 ms (98.55%), Space: 44.6 MB (20.43%) - LeetHub

--- 0764-n-ary-tree-level-order-traversal/0764-n-ary-tree-level-order-traversal.java
+++ 0764-n-ary-tree-level-order-traversal/0764-n-ary-tree-level-order-traversal.java
@@ -18,35 +18,24 @@ class Node {
 */
 
 class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+    HashMap<Integer,List<Integer>>map = new HashMap<>();
+    int mx=0;
     public List<List<Integer>> levelOrder(Node root) {
-        
-        Queue<Node> queue = new LinkedList<>();
-        List<List<Integer>> ans = new ArrayList<>();
         if(root==null)
             return ans;
-        queue.add(root);
-        queue.add(null);
-        int l=2;
-
-        while(l>1){
-            List<Integer> list1 = new ArrayList<>();
-            Node x=queue.poll();
-            l-=1;
-           
-            while(x!=null){
-                list1.add(x.val);
-                for(Node n:x.children){
-                    queue.add(n);
-                    l+=1;
-                   
-                }
-                    x=queue.poll();
-                    l-=1;
-            }
-            queue.add(null);
-            l+=1;
-            ans.add(list1);
-        }
+        dfs(root,0);
+        for(int i=0;i<=mx;i++)
+            ans.add(map.get(i));
         return ans;
+    }
+    public void dfs(Node root,int level){
+        mx=Math.max(mx,level);
+        if(map.get(level)==null)
+            map.put(level,new ArrayList<>());
+        map.get(level).add(root.val);
+        for(Node x:root.children){
+            dfs(x,level+1);
+        }
     }
 }
