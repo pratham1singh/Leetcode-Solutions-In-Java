921c149ba431fa0b0f7b6cd097c9a944a5568dd7
Pratham Singh <112713687+pratham1singh@users.noreply.github.com>
2024-03-19 18:40:41 +0530

    Time: 22 ms (58.39%), Space: 71 MB (98.59%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.java
+++ 1905-count-sub-islands/1905-count-sub-islands.java
@@ -1,30 +1,25 @@
 class Solution {
     int grid1[][],grid2[][],n,solved[][],m;
     public boolean find(int i,int j){
-            boolean ans=true;
-        //    System.out.println(i+" "+j);
-            if(i+1<m&&grid2[i+1][j]==1&&solved[i+1][j]==0){
-                solved[i+1][j]=1;
-                ans=find(i+1,j)&&ans;
-                // solved[i+1][j]=0;
-            }
-            if(i-1>=0&&grid2[i-1][j]==1&&solved[i-1][j]==0){
-                solved[i-1][j]=1;
-                ans=find(i-1,j)&&ans;
-                // solved[i-1][j]=0;
-            }
-            if(j+1<n&&grid2[i][j+1]==1&&solved[i][j+1]==0){
-                solved[i][j+1]=1;
-                ans=find(i,j+1)&&ans;
-                // solved[i][j+1]=0;
-            }
-            if(j-1>=0&&grid2[i][j-1]==1&&solved[i][j-1]==0){
-                solved[i][j-1]=1;
-                ans=find(i,j-1)&&ans;
-                // solved[i][j-1]=0;
-            }
-            ans=ans&&grid2[i][j]==1&&grid1[i][j]==1;
-            return ans;
+        boolean ans=true;
+        if(i+1<m&&grid2[i+1][j]==1&&solved[i+1][j]==0){
+            solved[i+1][j]=1;
+            ans=find(i+1,j)&&ans;
+        }
+        if(i-1>=0&&grid2[i-1][j]==1&&solved[i-1][j]==0){
+            solved[i-1][j]=1;
+            ans=find(i-1,j)&&ans;
+        }
+        if(j+1<n&&grid2[i][j+1]==1&&solved[i][j+1]==0){
+            solved[i][j+1]=1;
+            ans=find(i,j+1)&&ans;
+        }
+        if(j-1>=0&&grid2[i][j-1]==1&&solved[i][j-1]==0){
+            solved[i][j-1]=1;
+            ans=find(i,j-1)&&ans;
+        }
+        ans=ans&&grid2[i][j]==1&&grid1[i][j]==1;
+        return ans;
         
     }
     public int countSubIslands(int[][] grid1, int[][] grid2) {
@@ -35,19 +30,16 @@ class Solution {
         int visited[][]= new int[m][n],ans=0;
         solved= new int[m][n];
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        for(int i=0;i<m;i++)
+            for(int j=0;j<n;j++)
                 if(grid2[i][j]==1&&solved[i][j]==0){
                     // visited[i][j]=1;
                     solved[i][j]=1;
                     boolean res=find(i,j);
-                    if(res){
-                        // System.out.println(i+" "+j);
+                    if(res)
                         ans+=1;
-                    }
                 }
-            }
-        }
+            
         return ans;
     }
 }
