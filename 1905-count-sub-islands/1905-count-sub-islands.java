555326d9443a1881d47871f214a66d0d05386ef8
Pratham Singh <112713687+pratham1singh@users.noreply.github.com>
2024-03-19 18:39:36 +0530

    Time: 20 ms (77.67%), Space: 87.2 MB (23.73%) - LeetHub

---
+++ 1905-count-sub-islands/1905-count-sub-islands.java
@@ -0,0 +1,53 @@
+class Solution {
+    int grid1[][],grid2[][],n,solved[][],m;
+    public boolean find(int i,int j){
+            boolean ans=true;
+        //    System.out.println(i+" "+j);
+            if(i+1<m&&grid2[i+1][j]==1&&solved[i+1][j]==0){
+                solved[i+1][j]=1;
+                ans=find(i+1,j)&&ans;
+                // solved[i+1][j]=0;
+            }
+            if(i-1>=0&&grid2[i-1][j]==1&&solved[i-1][j]==0){
+                solved[i-1][j]=1;
+                ans=find(i-1,j)&&ans;
+                // solved[i-1][j]=0;
+            }
+            if(j+1<n&&grid2[i][j+1]==1&&solved[i][j+1]==0){
+                solved[i][j+1]=1;
+                ans=find(i,j+1)&&ans;
+                // solved[i][j+1]=0;
+            }
+            if(j-1>=0&&grid2[i][j-1]==1&&solved[i][j-1]==0){
+                solved[i][j-1]=1;
+                ans=find(i,j-1)&&ans;
+                // solved[i][j-1]=0;
+            }
+            ans=ans&&grid2[i][j]==1&&grid1[i][j]==1;
+            return ans;
+        
+    }
+    public int countSubIslands(int[][] grid1, int[][] grid2) {
+        m=grid1.length;
+        n=grid1[0].length;
+        this.grid1=grid1;
+        this.grid2=grid2;
+        int visited[][]= new int[m][n],ans=0;
+        solved= new int[m][n];
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid2[i][j]==1&&solved[i][j]==0){
+                    // visited[i][j]=1;
+                    solved[i][j]=1;
+                    boolean res=find(i,j);
+                    if(res){
+                        // System.out.println(i+" "+j);
+                        ans+=1;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
