3ade2196f069eb66c5ecc6ae6a3b67f44e84dbb8
Pratham Singh <112713687+pratham1singh@users.noreply.github.com>
2024-04-25 18:40:07 +0530

    Time: 9 ms (8.69%), Space: 43.8 MB (87.37%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.java
+++ 1094-car-pooling/1094-car-pooling.java
@@ -12,23 +12,27 @@ class Solution {
             return p.t-q.t;
         });
         Arrays.sort(trips,Comparator.comparingInt(a -> a[1]));
-        System.out.println(trips[0][1]);
         int c=capacity,i=0,k=0,n=trips.length;
+
         while(k<n){
+
             int []x=trips[k];
             while(pq.size()>0&&i==pq.peek().t){
                 c+=pq.peek().p;
                 pq.poll();
             }
             while(k<n&&i==trips[k][1]){
-                // System.out.println(i);
+                x=trips[k];
                 if(c<x[0])
-                return false;
+                    return false;
                 c-=x[0];
                 pq.add(new Trip(x[0],x[1],x[2]));
                 k+=1;
             }
-            i+=1;
+            if(pq.size()==0&&k<n)
+                i=trips[k][1];
+            else if(k<n)
+                i=Math.min(trips[k][1],pq.peek().t);
         }
         return true;
     }
