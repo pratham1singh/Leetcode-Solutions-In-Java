755885ea21ba932c1153dbdcf3d0f7d686a7d700
Pratham Singh <112713687+pratham1singh@users.noreply.github.com>
2024-04-25 18:36:53 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1094-car-pooling/1094-car-pooling.java
@@ -0,0 +1,35 @@
+class Trip{
+    int p,f,t;
+    public Trip(int p,int f ,int t){
+        this.p=p;
+        this.f=f;
+        this.t=t;
+    }
+}
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        PriorityQueue<Trip> pq = new PriorityQueue<>((p,q)->{
+            return p.t-q.t;
+        });
+        Arrays.sort(trips,Comparator.comparingInt(a -> a[1]));
+        System.out.println(trips[0][1]);
+        int c=capacity,i=0,k=0,n=trips.length;
+        while(k<n){
+            int []x=trips[k];
+            while(pq.size()>0&&i==pq.peek().t){
+                c+=pq.peek().p;
+                pq.poll();
+            }
+            while(k<n&&i==trips[k][1]){
+                // System.out.println(i);
+                if(c<x[0])
+                return false;
+                c-=x[0];
+                pq.add(new Trip(x[0],x[1],x[2]));
+                k+=1;
+            }
+            i+=1;
+        }
+        return true;
+    }
+}
