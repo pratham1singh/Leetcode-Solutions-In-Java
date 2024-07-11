993417ee74252cc9139f5707fbbc73e72f04a406
Pratham Singh <112713687+pratham1singh@users.noreply.github.com>
2024-07-11 15:30:20 +0530

    Time: 369 ms (13.82%), Space: 81.7 MB (5.31%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,22 +1,25 @@
 class Solution {
-     public int find2(int i,int[] arr,int n,int solved[]){
+    int solved[][];
+
+    public int find(int i,int pre,int nums[],int n){
         if(i==n)
             return 0;
-        else if(solved[i]!=-1)
-            return solved[i];
-        int ans=0;
-        for(int k=i+1;k<n;k++)
-            if(arr[k]>arr[i])
-                ans=Math.max(ans,1+find2(k,arr,n,solved));
-        return solved[i]=ans;
+        else if(pre==-1)
+            return Math.max(1+find(i+1,i,nums,n),find(i+1,pre,nums,n));
+        else if(solved[i][pre]!=-1)
+            return solved[i][pre];
+        else if(nums[i]>nums[pre])
+            return solved[i][pre]=Math.max(1+find(i+1,i,nums,n),find(i+1,pre,nums,n));
+        else
+            return solved[i][pre]=find(i+1,pre,nums,n);
     }
-    public int lengthOfLIS(int[] arr) {
-         int ans=0,n=arr.length;
-        int solved[]= new int[n];
-        // for(int x[]:solved)
-            Arrays.fill(solved,-1);
-       for(int i=0;i<n;i++)
-            ans=Math.max(ans,1+find2(i,arr,n,solved));
-        return ans;
+    public int lengthOfLIS(int[] nums) {
+        int n=nums.length;
+        solved= new int[n][n];
+        for(int x[]:solved)
+            Arrays.fill(x,-1);
+
+        return find(0,-1,nums,n);
+
     }
 }
