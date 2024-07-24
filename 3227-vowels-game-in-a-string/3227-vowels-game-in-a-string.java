d872f85cbcb3b77b05ba911a1b6fd7e6efd524ce
Pratham Singh <112713687+pratham1singh@users.noreply.github.com>
2024-07-24 23:40:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.java
@@ -0,0 +1,39 @@
+class Solution {
+
+    //flag==1 
+    int visited[][];
+    public boolean find(int n,int flag){
+        if(n==0&&flag==1)
+            return false;
+        if(n==0)
+            return true;
+        if(visited[n][flag]!=-1)
+            return visited[n][flag]==1;
+        if(flag==1){
+            boolean ans=false;
+            for(int k=1;k<n;k+=2)
+                ans=ans || find(n-k,0);
+            visited[n][flag]=(ans?1:0);
+            return ans;
+        }
+        else{
+            boolean ans=true;
+            for(int k=1;k<n;k+=2)
+                ans=ans&&find(n-k,0);
+            visited[n][flag]=(ans?1:0);
+            return ans;
+        }
+
+    }
+    public boolean doesAliceWin(String s) {
+        int n=0;
+        for(int i=0;i<s.length();i++)
+            if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u')
+                n++;
+        visited= new int[n+1][2];
+        for(int x[]:visited)
+            Arrays.fill(x,-1);
+
+        return find(n,1);
+    }
+}
