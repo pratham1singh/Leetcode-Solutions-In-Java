2a0a7944730842949d71baddb7c5841808984adb
Pratham Singh <112713687+pratham1singh@users.noreply.github.com>
2024-07-24 23:40:18 +0530

    Time: 20 ms (23.79%), Space: 45.6 MB (38.29%) - LeetHub

--- 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.java
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.java
@@ -7,19 +7,23 @@ class Solution {
             return false;
         if(n==0)
             return true;
+        if(n%2==0&&flag==0)
+            return false;
+        if(n%2!=0&&flag==1)
+            return true;
         if(visited[n][flag]!=-1)
             return visited[n][flag]==1;
         if(flag==1){
             boolean ans=false;
-            for(int k=1;k<n;k+=2)
+            for(int k=1;k<=n;k+=2)
                 ans=ans || find(n-k,0);
             visited[n][flag]=(ans?1:0);
             return ans;
         }
         else{
             boolean ans=true;
-            for(int k=1;k<n;k+=2)
-                ans=ans&&find(n-k,0);
+            for(int k=2;k<=n;k+=2)
+                ans=ans&&find(n-k,1);
             visited[n][flag]=(ans?1:0);
             return ans;
         }
