42e5f327fb1c954769827381c9c373e90ceb4c80
Pratham Singh <112713687+pratham1singh@users.noreply.github.com>
2024-08-14 22:52:41 +0530

    Time: 496 ms (5.49%), Space: 140.7 MB (22.98%) - LeetHub

---
+++ 3249-count-the-number-of-good-nodes/3249-count-the-number-of-good-nodes.java
@@ -0,0 +1,43 @@
+class Solution {
+    HashMap<Integer,Integer> children= new HashMap<>();
+    HashMap<Integer,ArrayList<Integer>> adj= new HashMap<>();
+    int visited[];
+
+    public int countChildren(int root){
+        int ans=0;
+        visited[root]=1;
+        for(int x:adj.get(root))
+            if(visited[x]==0)
+                ans+=countChildren(x);
+        visited[root]=0;
+        children.put(root,ans+1);
+        return ans+1;
+    }
+    public int countGoodNodes(int root){
+       int ans=0;
+       HashSet<Integer> set = new HashSet<>();
+       visited[root]=1;
+       for(int x:adj.get(root))
+            if(visited[x]==0){
+                ans+=countGoodNodes(x);
+                set.add(children.get(x));
+            }
+       visited[root]=0;
+       return (set.size()<=1?1:0)+ans;
+       
+    }
+    public int countGoodNodes(int[][] edges) {
+        int n=edges.length+1;
+        visited= new int[n];
+        for(int i=0;i<n;i++)
+            adj.put(i, new ArrayList<>());
+
+        for(int x[]:edges){
+            adj.get(x[0]).add(x[1]);
+            adj.get(x[1]).add(x[0]);
+        }
+        countChildren(0);
+        // System.out.println(adj);
+        return countGoodNodes(0);
+    }
+}
