2e2af84984d9bf8c4fb15b112e3b953efb1c69e1
Pratham Singh <112713687+pratham1singh@users.noreply.github.com>
2026-06-07 16:23:53 +0530

    Time: 3 ms (88.78%), Space: 47.5 MB (67.59%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,41 +1,23 @@
-class Node {
-    int val, ind;
-
-    Node(int ind, int val) {
-        this.val = val;
-        this.ind = ind;
-    }
-}
-
 class Solution {
     public int candy(int[] arr) {
-        int n = arr.length;
-        PriorityQueue<Node> pq = new PriorityQueue<>((a,b)-> a.val-b.val);
+        int n= arr.length, temp=0;
         int[] ans = new int[n];
 
-        Arrays.fill(ans, 1);
+        Arrays.fill(ans,1);
 
-        for(int i=0;i<n;i++) pq.add(new Node(i, arr[i]));
-
-        while(pq.size() > 0) {
-            Node node = pq.remove();
-            int i = node.ind -1, j= node.ind, k = node.ind+1, mx =-1;
-
-            if(i>=0 &&  arr[i] < arr[j]) {
-                mx= ans[i] + 1;
-            }
-
-            if(k<n && arr[j] > arr[k]) {
-                mx = Math.max(mx, ans[k] + 1);
-            }
-            
-            if(mx!=-1)
-                ans[j] = mx;
+        for(int i=0;i<n-1;i++) {
+            if(arr[i]<arr[i+1]) ans[i+1]=ans[i]+1;
         }
 
-        int temp = 0;
+        for(int i=n-1;i>0;i--) {
+            if(arr[i-1]>arr[i]) ans[i-1] = ans[i] +1;
+        }
 
+        for(int i=1;i<n-1;i++) {
+            if(arr[i-1] < arr[i] && arr[i] > arr[i+1]) ans[i] = Math.max(ans[i-1], ans[i+1]) + 1;
+        }
         for(int x: ans) temp+=x;
+
         return temp;
     }
 }
