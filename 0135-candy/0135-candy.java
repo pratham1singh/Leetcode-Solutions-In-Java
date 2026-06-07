b431c5b0ac30b944729fb0c62f2af9c770739834
Pratham Singh <112713687+pratham1singh@users.noreply.github.com>
2026-06-07 16:32:53 +0530

    Time: 4 ms (23.02%), Space: 47.9 MB (6.75%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -11,11 +11,13 @@ class Solution {
 
         for(int i=n-1;i>0;i--) {
             if(arr[i-1]>arr[i]) ans[i-1] = ans[i] +1;
+
+            if(i+1<n && arr[i-1] < arr[i] && arr[i] > arr[i+1]) ans[i] = Math.max(ans[i-1], ans[i+1]) + 1;
         }
 
-        for(int i=1;i<n-1;i++) {
-            if(arr[i-1] < arr[i] && arr[i] > arr[i+1]) ans[i] = Math.max(ans[i-1], ans[i+1]) + 1;
-        }
+        // for(int i=1;i<n-1;i++) {
+        //     if(arr[i-1] < arr[i] && arr[i] > arr[i+1]) ans[i] = Math.max(ans[i-1], ans[i+1]) + 1;
+        // }
         for(int x: ans) temp+=x;
 
         return temp;
