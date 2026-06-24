a96ff3cb19fa792f07499a782548dd8995166629
Pratham Singh <112713687+pratham1singh@users.noreply.github.com>
2026-06-24 23:07:18 +0530

    Time: 14 ms (54.7%), Space: 46.7 MB (52.36%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -13,21 +13,21 @@ class Solution {
         int n = s.length();
         HashMap<Character, Integer> map = new HashMap<>();
         PriorityQueue<Node> pq = new PriorityQueue<>((a,b) -> b.count - a.count);
-        String ans = "";
+        StringBuilder ans = new StringBuilder();
 
         for(int i=0;i<n;i++) {
             map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
         }
         
         for(Character ch: map.keySet()) pq.add(new Node(ch, map.get(ch)));
-
+        
         while(pq.size() > 0) {
             Node temp = pq.remove();
 
-            while(temp.count-- >0) ans+=temp.ch;
+            while(temp.count-- >0) ans.append(temp.ch);
         }
 
-        return ans;
+        return ans.toString();
 
     }
 }
