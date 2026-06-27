05e08634dcdeba8cd31e677d9b6959ea789be385
Pratham Singh <112713687+pratham1singh@users.noreply.github.com>
2026-06-27 11:18:45 +0530

    Time: 24 ms (9.72%), Space: 47.5 MB (95.67%) - LeetHub

--- 3968-maximum-manhattan-distance-after-all-moves/3968-maximum-manhattan-distance-after-all-moves.java
+++ 3968-maximum-manhattan-distance-after-all-moves/3968-maximum-manhattan-distance-after-all-moves.java
@@ -1,18 +1,16 @@
 class Solution {
     public int maxDistance(String str) {
-        int u =  0, d = 0, l = 0, r=0, n=str.length(), e =0, ans=0;
+        int a=0, b=0, n=str.length(), e =0;
+
         for(int i=0;i<n;i++) {
-            if(str.charAt(i) == 'U') u++;
-            else if(str.charAt(i) == 'D') d++;
-            else if(str.charAt(i) == 'L') l++;
-            else if (str.charAt(i) == 'R') r++;
+            if(str.charAt(i) == 'U') a++;
+            else if(str.charAt(i) == 'D') a--;
+            else if(str.charAt(i) == 'L') b++;
+            else if (str.charAt(i) == 'R') b--;
+            else e++;
         }
 
-        e = n- (u+d+l+r);
-        
-        ans = Math.abs(u-d) + Math.abs(l-r) + e;
-
-        return ans;
+        return Math.abs(a) + Math.abs(b) + e;
 
     }
 }
