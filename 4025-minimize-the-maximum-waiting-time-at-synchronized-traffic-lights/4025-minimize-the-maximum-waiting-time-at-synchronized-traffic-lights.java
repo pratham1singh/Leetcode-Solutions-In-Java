8a117d9e52a97a945cb61b2eef9536064f1f78b0
Pratham Singh <112713687+pratham1singh@users.noreply.github.com>
2026-08-22 11:38:40 +0530

    Time: 3 ms (99.8%), Space: 121.9 MB (29.91%) - LeetHub

--- 4025-minimize-the-maximum-waiting-time-at-synchronized-traffic-lights/4025-minimize-the-maximum-waiting-time-at-synchronized-traffic-lights.java
+++ 4025-minimize-the-maximum-waiting-time-at-synchronized-traffic-lights/4025-minimize-the-maximum-waiting-time-at-synchronized-traffic-lights.java
@@ -2,13 +2,9 @@ class Solution {
     public int minPenalty(int period, int[] lights, int[] arrivalTime) {
         int n = arrivalTime.length, mxL = Integer.MIN_VALUE, mxA = Integer.MAX_VALUE;
 
-        for(int i=0;i<n;i++) arrivalTime[i] = arrivalTime[i]%period;
-
         for(int x: lights) if(x> mxL) mxL = x;
 
-        for(int i=0;i<n;i++) if(arrivalTime[i] < mxL) arrivalTime[i]= Integer.MAX_VALUE;
-
-         for(int x: arrivalTime) if(x < mxA) mxA = x;
+        for(int i=0;i<n;i++) if(arrivalTime[i]%period>= mxL && arrivalTime[i]%period < mxA)  mxA = arrivalTime[i]%period;
 
          return (period - mxA) <  0 ? 0 : (period - mxA);
 
