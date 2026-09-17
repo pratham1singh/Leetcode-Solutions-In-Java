fd7b4a7989a511223a5eac95640f1cdc1ab60e62
Pratham Singh <112713687+pratham1singh@users.noreply.github.com>
2026-09-17 09:43:45 +0530

    Time: 675 ms (5.03%), Space: 43 MB (78.91%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,19 +1,17 @@
 class Solution {
+    int n;
 
-    int n,target;
-    public int find(int i,int value,int nums[]){
-        if(i==n&&value==target)
-            return 1;
-        if(i==n)
-            return 0;
-        int ans=0;
-        ans+=find(i+1,value+nums[i],nums);
-        ans+=find(i+1,value-nums[i],nums);
-        return ans;
+    public int find(int i, int target, int[] nums) {
+        if(target==0 && i==n) return 1;
+        if(i==n) return 0;
+
+        return find(i+1, target+nums[i], nums) + find(i+1, target-nums[i], nums);
+
     }
+
     public int findTargetSumWays(int[] nums, int target) {
-        n=nums.length;
-        this.target=target;
-        return find(0,0,nums);
+        n = nums.length;
+
+        return find(0,target,nums);
     }
 }
